10d13a9d19ac86b56c17cca5d764a1155d3f17ab
kstoriadis <federico.gonzalezc@gmail.com>
2015-07-27 12:24:15 -0300

    bug
    bug

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,7 @@ class AppKernel extends Kernel
 
             new ContadoresBundle\ContadoresBundle(),
 
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            //new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
