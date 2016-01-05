eb30e43d7e031e473b5c51a929a93d3e14d15a3f
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-01-05 09:36:29 -0300

    cuit mask en filter

--- src/ContadoresBundle/Resources/public/assets/js/cuit_cliente.js
+++ src/ContadoresBundle/Resources/public/assets/js/cuit_cliente.js
@@ -1,19 +1,18 @@
 (function(window, document, $) {
-  console.log("hola");
-   $("#contadoresbundle_cliente_cuit")
-    .inputmask(
-      "99-99999999-9",
-      {
-        "removeMaskOnSubmit": true,
-        "clearMaskOnLostFocus": false,
-      }
-    );
-    // $("#choferesbundle_prestadorfiltertype_cuit")
-    //  .inputmask(
-    //    "99-99999999-9",
-    //    {
-    //      "removeMaskOnSubmit": true,
-    //      "clearMaskOnLostFocus": false,
-    //    }
-    //  );
+  $("#contadoresbundle_cliente_cuit")
+  .inputmask(
+    "99-99999999-9",
+    {
+      "removeMaskOnSubmit": true,
+      "clearMaskOnLostFocus": false,
+    }
+  );
+  $("#contadoresbundle_clientefiltertype_cuit")
+   .inputmask(
+     "99-99999999-9",
+     {
+       "removeMaskOnSubmit": true,
+       "clearMaskOnLostFocus": false,
+     }
+   );
 })(window, document, jQuery);
