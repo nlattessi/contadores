b98cdc90374cf1be776540a4a1a77a44b23916a6
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-01-14 18:55:53 -0300

    archivo en tarea metadata

--- src/ContadoresBundle/Controller/TareaMetadataController.php
+++ src/ContadoresBundle/Controller/TareaMetadataController.php
@@ -123,6 +123,18 @@ class TareaMetadataController extends Controller
             $em = $this->getDoctrine()->getManager();
             $em->persist($entity);
             $em->flush();
+
+            if (! empty($form['attachment']->getData())) {
+                $archivoService = $this->get('contadores.servicios.archivo');
+                foreach ($form['attachment']->getData() as $archivo) {
+                    $archivoService->createArchivoTareaMetadata(
+                        $archivo,
+                        $this->getUser(),
+                        $entity
+                    );
+                }
+            }
+
             $this->get('session')->getFlashBag()->add('success', 'flash.create.success');
 
             return $this->redirect($this->generateUrl('tareametadata_show', array('id' => $entity->getId())));
